3d1ff6304c2394c010792089d7a11f9468def1ea
Aniket Pingley <81234187+aniket-acad@users.noreply.github.com>
2021-06-03 09:54:50 +0530

    Update linked_list.c

--- adt/linked_list.c
+++ adt/linked_list.c
@@ -38,13 +38,49 @@ status_t prependNode(nodeQ_t* head, nodeQ_t* newNode){
     return LIST_TRUE;
 }
 
+void printList(nodeQ_t* head){
+    if(EMPTYNODE == head) return;
+
+    nodeQ_t* node = head;
+    while(node){
+        printf("%d --> ", node->data);
+        node = node->next;
+    }
+    printf("EMPTYNODE\n");
+}
+
+void destroyList(nodeQ_t* ptr){
+    if(EMPTYNODE == ptr) return;
+
+    while(ptr){
+        nodeQ_t* current = ptr;
+        printList(current);
+        ptr = ptr->next;
+        free(current);    
+    }
+
+    printf("EMPTYNODE\n");
+}
+
+status_t isDataPresent(nodeQ_t* ptr, short data){
+    if(EMPTYNODE == ptr) return LIST_FALSE;
+
+    while(ptr){
+        if(data == ptr->data) return LIST_TRUE;
+        ptr = ptr->next;
+    }
+
+    return LIST_FALSE;
+}
 
 int main(){
-
+    
     nodeQ_t* head = EMPTYNODE;
     nodeQ_t* tail = EMPTYNODE;
-
+    
     head = createNode(20);
+    printf("%p\n", head);
+
     tail = head;
 
     for(short i=1; i < 6; i++){
@@ -59,11 +95,21 @@ int main(){
             head = newNode;
     }
 
-
+    #if 0
     printf("%d\t\t%p\t\t%p\n", head->data, head, head->next);
     printf("%d\t\t%p\t\t%p\n", head->next->data, head->next, head->next->next);
     printf("%d\t\t%p\t\t%p\n", head->next->next->data, head->next->next, head->next->next->next);
     printf("%d\t\t%p\t\t%p\n", tail->data, tail, tail->next);
+    #endif
+
+    //printList(head);
+
+    printf("dataPresent = %d \n", isDataPresent(head, 225));
+
+    //destroyList(head);
+    //head = EMPTYNODE;
+
+    //printf("%d\n", head->data);
 
     return 0;
 }
