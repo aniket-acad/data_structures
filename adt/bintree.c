e17cf8afb82c7732a59fa4caf8e60685abf60e78
Aniket Pingley <81234187+aniket-acad@users.noreply.github.com>
2021-06-25 14:24:56 +0530

    Update bintree.c

--- adt/bintree.c
+++ adt/bintree.c
@@ -108,12 +108,96 @@ void printPostorder(nodeQ_t* node){
 
 }
 
+nodeQ_t* getParentNodeBST(nodeQ_t* node, short val){
 
+    while(EMPTYNODE != node){
+        if ((val > node->data) && (node->right != EMPTYNODE))     
+            node = node->right;
+        else if ((val > node->data) && (node->right == EMPTYNODE))
+            return node;
+        else if ((val < node->data) && (node->left != EMPTYNODE))    
+            node = node->left;
+        else if ((val < node->data) && (node->left == EMPTYNODE))
+            return node;
+        else
+            return EMPTYNODE;
+    }
+
+    return EMPTYNODE;
+}
+
+
+void insertNodeInBST(nodeQ_t* root, nodeQ_t* newNode){
+
+    if(EMPTYNODE == root || EMPTYNODE == newNode)
+        return;
+
+    nodeQ_t* parent = getParentNodeBST(root, newNode->data);
+    if(EMPTYNODE == parent)
+        return;
+
+    if(newNode->data > parent->data)
+        parent->right = newNode;
+    if(newNode->data < parent->data)
+        parent->left = newNode;
+}
+
+
+nodeQ_t* minNode(nodeQ_t* node){
+    if(EMPTYNODE == node)
+        return EMPTYNODE;
+
+    nodeQ_t* temp = node;
+    while(EMPTYNODE != temp->left){
+        temp = temp->left;
+    }
+
+    return temp;
+}
+
+
+
+nodeQ_t* deleteNode(nodeQ_t* root, int key)
+{
+    if (root == EMPTYNODE)
+        return root;
+    
+    if (key < root->data)
+        root->left = deleteNode(root->left, key);
+ 
+    else if (key > root->data)
+        root->right = deleteNode(root->right, key);
+ 
+    else {
+        // key == root->data is TRUE
+        // node with only one child or no child
+        if (root->left == EMPTYNODE) {
+            nodeQ_t* temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == EMPTYNODE) {
+            nodeQ_t* temp = root->left;
+            free(root);
+            return temp;
+        }
+ 
+        // both children for the node are present
+        nodeQ_t* mNode = minNode(root->right);
+
+        if(EMPTYNODE != mNode)
+        {
+            root->data = mNode->data;
+            root->right = deleteNode(root->right, mNode->data);
+        }
+    }
+    return root;
+}
 
 int main()
 {
 	/*create root*/
-	struct node* root = createNode(11);
+	/*struct node* root = createNode(11);
 
 	root->left = createNode(22);
 	root->right = createNode(33);
@@ -131,7 +215,7 @@ int main()
     root->right->right->right = createNode(77);
 
     root->left->left->left = createNode(-45);
-    root->left->right->right = createNode(-9);
+    root->left->right->right = createNode(-9);*/
 
     /*printf("%d\n", root->data);
     printf("%d\n", root->left->data);
@@ -141,7 +225,7 @@ int main()
     printf("%d\n", root->right->left->data);
     printf("%d\n", root->right->right->data);*/
 
-    printTree(root, 0);
+    //printTree(root, 0);
 
     /*trackerIdx = 0;
     printPath(root, 55);
@@ -152,6 +236,7 @@ int main()
     trackerIdx = 0;
     printPath(root, 222);*/
 
+    /*
     printf("Inorder: ");
     printInorder(root);
     printf("\n");
@@ -161,6 +246,43 @@ int main()
     printf("Postorder: ");
     printPostorder(root);
     printf("\n");
+    */
+
+   nodeQ_t* root = createNode(100);
+   insertNodeInBST(root, createNode(100));
+   insertNodeInBST(root, createNode(104));
+   insertNodeInBST(root, createNode(56));
+   insertNodeInBST(root, createNode(23));
+   insertNodeInBST(root, createNode(23));
+   insertNodeInBST(root, createNode(200));
+   insertNodeInBST(root, createNode(292));
+   insertNodeInBST(root, createNode(912));
+   insertNodeInBST(root, createNode(5));
+   insertNodeInBST(root, createNode(17));
+   insertNodeInBST(root, createNode(211));
+   insertNodeInBST(root, createNode(211));
+   insertNodeInBST(root, createNode(101));
+   insertNodeInBST(root, createNode(-95));
+
+   printTree(root, 0);
+
+   nodeQ_t *mNode = minNode(root);
+   printf("min node = %d\n", mNode->data);
+
+   root = deleteNode(root, 1000);
+   printTree(root, 0);
+
+
+   /*nodeQ_t* rootDegn = createNode(100);
+   insertNodeInBST(rootDegn, createNode(101));
+   insertNodeInBST(rootDegn, createNode(102));
+   insertNodeInBST(rootDegn, createNode(105));
+   insertNodeInBST(rootDegn, createNode(123));
+   insertNodeInBST(rootDegn, createNode(134));
+   insertNodeInBST(rootDegn, createNode(200));
+
+   printTree(rootDegn, 0);
+    */
 	
 	return 0;
 }
